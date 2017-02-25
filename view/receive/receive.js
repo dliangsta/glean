c8374d085c7abc78b11c9c9041cd0d4f56511f34
Evan Hernandez <evanfredhernandez@gmail.com>
2017-02-25 14:43:17 -0600

    More frontend stuff.

--- view/receive/receive.js
+++ view/receive/receive.js
@@ -1,4 +1,5 @@
 angular.module('glean')
-  .controller('ReceiveController', function() {
-    // TODO.
+  .controller('ReceiveController', function($scope) {
+    $scope.orgs = [{name:'Shelter 1'}, {name:'Shelter 2'}];
+    $scope.deliveries = [{restaurantID:'McDonlads', description:'Ten big macs', quantity:10, notes:'We like big macs'}];
   });
